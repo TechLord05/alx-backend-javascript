e08d5917f99f3058b9f8c984d2e7f4e00b457126
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2024-04-24 14:35:26 +0000

    Refactor getBudgetObject function to use object property value shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,11 +1,3 @@
-/**
- * Returns an object representing budget using object property value shorthand syntax.
- *
- * @param {number} income - The income value.
- * @param {number} gdp - The gdp value.
- * @param {number} capita - The capita value.
- * @returns {Object} - The budget object.
- */
 export default function getBudgetObject(income, gdp, capita) {
     const budget = {
       income,
