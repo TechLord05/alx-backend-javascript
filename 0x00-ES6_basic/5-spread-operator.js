2359b3ba1937bc62e79a1943dbf16c3f22acfc16
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2024-04-24 10:39:53 +0000

    The wonders of spread syntax

---
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -0,0 +1,13 @@
+
+/**
+ * Concatenates two arrays and each character of a string using spread syntax.
+ *
+ * @param {Array} array1 - The first array to concatenate.
+ * @param {Array} array2 - The second array to concatenate.
+ * @param {string} string - The string to concatenate.
+ * @returns {Array} - The concatenated array.
+ */
+export default function concatArrays(array1, array2, string) {
+    return [...array1, ...array2, ...string];
+  }
+  
