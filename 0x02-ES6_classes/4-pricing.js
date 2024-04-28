e97a5d5d72a4e283f4ca924f34cf1e8bbb199876
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2024-04-28 12:08:14 +0000

    Implement a class named Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+import Currency from './3-currency.js';
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    set amount(value) {
+        this._amount = value;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set currency(newCurrency) {
+        this._currency = newCurrency;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
